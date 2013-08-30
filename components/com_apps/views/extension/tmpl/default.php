e4c666ce45d10025777a08128ac177496d8fb4f2
Efthimios Mavrogeorgiadis <emavro@yahoo.com>
2013-08-30 17:34:16 +0300

    Automatically load CSS & JS files

--- components/com_apps/views/extension/tmpl/default.php
+++ components/com_apps/views/extension/tmpl/default.php
@@ -17,10 +17,6 @@ $advanced_search	 	= new JLayoutFile('joomla.apps.advanced_search');
 $simple_search			= new JLayoutFile('joomla.apps.simple_search');
 $extension_data			= array('extensions' => $this->extensions, 'breadcrumbs' => $this->breadcrumbs, 'params' => $this->params);
 ?>
-<script type="text/javascript" src="<?php echo JURI::root(); ?>components/com_apps/views/dashboard/js/jquery.jscrollpane.min.js"></script>
-<script type="text/javascript" src="<?php echo JURI::root(); ?>components/com_apps/views/dashboard/js/jquery.mousewheel.js"></script>
-<script type="text/javascript" src="<?php echo JURI::root(); ?>components/com_apps/views/dashboard/js/jquery.japps.js"></script>
-
 <div class="com-apps-container">
 <div class="row-fluid">
 	<div class="span3">
