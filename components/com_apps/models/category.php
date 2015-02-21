ebb190eabf8a2497227772ed78579d431886ac59
Ashwin Date <coolbung@gmail.com>
2015-02-21 22:03:55 +0530

    Extensions by category now works

--- components/com_apps/models/category.php
+++ components/com_apps/models/category.php
@@ -35,17 +35,17 @@ class AppsModelCategory extends JModelList
 	private $_parent = null;
 
 	private $_items = null;
-	
+
 	private $_remotedb = null;
-	
+
 	public $_extensionstitle = null;
-	
+
 	private $_categories = null;
-	
+
 	private $_breadcrumbs = array();
-	
+
 	private $_total = null;
-	
+
 	private $_orderby = 't2.link_rating';
 
 	/**
@@ -134,35 +134,18 @@ class AppsModelCategory extends JModelList
 		return $this->_parent;
 	}
 
-	private function getRemoteDB() {
-		if (!is_object($this->_remotedb)) {
-			jimport('joomla.application.component.helper');
-			$componentParams = JComponentHelper::getParams('com_apps');
-
-			$fields = array('driver', 'host', 'user', 'password', 'database', 'port');
-			$options = array();
-		
-			foreach ($fields as $field) {
-				$options[$field] = $componentParams->get($field);
-			}
-		
-			$this->_remotedb = JDatabaseDriver::getInstance( $options );
-		}
-		return $this->_remotedb;
-	}
-	
 	private function getBaseModel()
 	{
 		$base_model = JModelLegacy::getInstance('Base', 'AppsModel');
 		return $base_model;
 	}
-	
+
 	private function getCatID()
 	{
 		$input = new JInput;
 		return $input->get('id', null, 'int');
 	}
-	
+
 	public function getCategories()
 	{
 		$base_model = $this->getBaseModel();
@@ -174,200 +157,86 @@ class AppsModelCategory extends JModelList
 		$base_model = $this->getBaseModel();
 		return $base_model->getBreadcrumbs($this->getCatID());
 	}
-	
+
 	public function getPluginUpToDate()
 	{
 		$base_model = $this->getBaseModel();
 		return $base_model->getPluginUpToDate();
 	}
-	
+
 	public function getOrderBy()
 	{
 		return $this->_orderby;
 	}
-	
-	private function loadExtensionIDs($db, $catid)
+
+	public function getExtensions()
 	{
 		// Get catid, search filter, order column, order direction
+		$cache 						= JFactory::getCache();
+		$cache->setCaching( 1 );
+		$http 						= new JHttp;
 		$componentParams 	= JComponentHelper::getParams('com_apps');
-		$default_limit		= $componentParams->get('default_limit');
-		$input 				= new JInput;
-		$release			= preg_replace('/[^\d]/', '', base64_decode($input->get('release', '', 'base64')));
-		$limitstart 		= $input->get('limitstart', 0, 'int');
-		$limit 				= $input->get('limit', $default_limit, 'int');
-		$order 				= $input->get('ordering', 't2.link_hits');
-		$search 			= str_replace('_', ' ', urldecode($input->get('filter_search', null)));
-		$orderCol 			= $this->state->get('list.ordering', $order);
-		$orderDirn 			= $orderCol == 't2.link_name' ? 'ASC' : 'DESC';
-		$order 				= $orderCol.' '.$orderDirn;
+		$api_url 					= new JUri;
+		$default_limit		= $componentParams->get('default_limit', 8);
+		$input 						= new JInput;
+		$catid 						= $input->get('id', null, 'int');
+		$order 						= $input->get('ordering', $this->getOrderBy());
+		$orderCol 				= $this->state->get('list.ordering', $order);
+		$orderDirn 				= $orderCol == 'core_title' ? 'ASC' : 'DESC';
+		$order 						= $this->getBaseModel()->getOrder($orderCol, $orderDirn);
+		$release					= preg_replace('/[^\d]/', '', base64_decode($input->get('release', '', 'base64')));
+		$limitstart 			= $input->get('limitstart', 0, 'int');
+		$limit 						= $input->get('limit', $default_limit, 'int');
+		$dashboard_limit	= $componentParams->get('extensions_perrow') * 6; // 6 rows of extensions
+		$search 					= str_replace('_', ' ', urldecode(trim($input->get('filter_search', null))));
 
 		$release = intval($release / 5) * 5;
 
-		$query = 'SET SESSION group_concat_max_len=150000';
-		$db->setQuery($query);
-		$db->execute();
-		
-		// Get category name
-		if ($catid) {
-			$query = $db->getQuery(true);
-			$query->select('cat_name');
-			$query->from('jos_mt_cats');
-			$query->where('cat_id = ' . (int)$catid);
-			$db->setQuery($query);
-			$catname = $db->loadResult();
-			if ($catname) {
-				$this->_extensionstitle = $catname;
-			}
-		}
-		
-		// Form query
-		$query = $db->getQuery(true);
-		$query->select(array('t2.link_id AS id'));
-		$query->from('jos_mt_cl AS t1');
-		$query->join('LEFT', 'jos_mt_links AS t2 ON t1.link_id = t2.link_id');
-		$query->join('RIGHT', 'jos_mt_cfvalues AS t3 ON t3.link_id = t2.link_id AND t3.cf_id = 37 AND ("'.$release.'" REGEXP t3.value OR t3.value = "")');
+		$api_url->setScheme('http');
+		$api_url->setHost('extensions.joomla.org/index.php');
+		$api_url->setvar('option', 'com_jed');
+		$api_url->setvar('controller', 'filter');
+		$api_url->setvar('view', 'extension');
+		$api_url->setvar('format', 'json');
+		$api_url->setvar('limit', $limit);
+		$api_url->setvar('limitstart', $limitstart);
+		$api_url->setvar('filter[approved]', '1');
+		$api_url->setvar('filter[published]', '1');
+		$api_url->setvar('filter[core_catid]', $catid);
+		$api_url->setvar('extend', '0');
+		$api_url->setvar('order', $orderCol);
+		$api_url->setvar('dir', $orderDirn);
 
-		if (!$order) {
-			$order = 't2.link_hits DESC';
-		}
+		if ($search)
+		$api_url->setvar('searchall', $search);
 
-		$where = array();
-		if ($catid && !$search) {
-			$where[] = 't1.cat_id IN (' . implode(',', $catid) . ')';
-		}
+		$extensions_json = $cache->call(array($http, 'get'), $api_url);
 
-		if ($search) {
-			$where[] = '(t2.link_name LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . ') OR t2.link_desc LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . '))';
-		}
-		
-		$where = array_merge($where, array(
-			't2.link_published = 1',
-			't2.link_approved = 1',
-			'(t2.publish_up <= NOW() OR t2.publish_up = "0000-00-00 00:00:00")',
-			'(t2.publish_down >= NOW() OR t2.publish_down = "0000-00-00 00:00:00")',
-		));
-
-		$query->where($where);
-		$query->order($order);
-		$query->group('t2.link_id');
-		$db->setQuery($query, $limitstart, $limit);
-		return $db->loadColumn();
-	}
-	
-	public function getExtensions()
-	{
-		
-		return array();
-		
-		// Get catid, search filter, order column, order direction
-		$componentParams 	= JComponentHelper::getParams('com_apps');
-		$input 				= new JInput;
-		$catid 				= $input->get('id', null, 'int');
-		$search 			= str_replace('_', ' ', urldecode($input->get('filter_search', null)));
-		
-		// Get remote database
-		$db = $this->getRemoteDB();
-		$ids = $this->loadExtensionIDs($db, array($catid));
-		
-		if (!count($ids)) {
-			$this->_orderby = 't2.link_hits';
-			$base_model = $this->getBaseModel();
-			$children = $base_model->getChildren($catid);
-			$catid = $this->getAllChildren($children, $catid);
-			if (count($catid)) {
-				$ids = $this->loadExtensionIDs($db, $catid);
-			}
-		}
-
-		$order 				= $input->get('ordering', $this->getOrderBy());
-		$orderCol 			= $this->state->get('list.ordering', $order);
-		$orderDirn 			= $orderCol == 't2.link_name' ? 'ASC' : 'DESC';
-		$order 				= $this->getBaseModel()->getOrder($orderCol, $orderDirn);
-
-		$items = array();	
-		if (count($ids)) {
-			$query = $db->getQuery(true);
-			$fields = array(
-				't2.link_id AS id',
-				't2.link_id',
-				't2.link_name AS name',
-				't2.alias AS alias',
-				't2.link_desc AS description',
-				't2.link_rating AS rating',
-				't2.user_id AS user_id',
-				't2.link_votes',
-				't3.filename AS image',
-				't1.cat_id AS cat_id',
-				'CONCAT("{", GROUP_CONCAT(DISTINCT "\"", t5.cf_id, "\":\"", t4.value, "\""), "}") AS options',
-				'COUNT(DISTINCT t7.rev_id) AS reviews',
-			);
-			if (preg_match('/^t2\.link_rating/', $order)) {
-				$fields[] = 'IF(t2.link_votes >= 5, 1, 0) as votelimit';
-			}
-			if ($search) {
-				$fields[] = 'IF(t2.link_name LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . '), 1, 0) as foundintitle';
-			}
-			$query->select($fields);
-
-			$query->from('jos_mt_cl AS t1');
-			$query->join('LEFT', 'jos_mt_links AS t2 ON t1.link_id = t2.link_id');
-			$query->join('LEFT', 'jos_mt_images AS t3 ON t3.link_id = t2.link_id');
-			$query->join('LEFT', 'jos_mt_cfvalues AS t4 ON t2.link_id = t4.link_id');
-			$query->join('LEFT', 'jos_mt_customfields AS t5 ON t4.cf_id = t5.cf_id');
-			$query->join('LEFT', 'jos_mt_reviews AS t7 ON t7.link_id = t2.link_id');
-
-			$query->where(array(
-				't2.link_id IN (' . implode(',', $ids) . ')',
-			));
-			$query->order($order);
-			$query->group('t2.link_id');
-			$db->setQuery($query);
-			$items = $db->loadObjectList();
-		}
+		$items = json_decode($extensions_json->body);
+		$items = $items->data;
 
 		// Get CDN URL
 		$cdn = preg_replace('#/$#', '', trim($componentParams->get('cdn'))) . "/";
-		
+
 		// Populate array
 		$extensions = array(0=>array(), 1=>array());
-		if (preg_match('/^t2\.link_rating/', $order)) {
-			$extensions[0] = array(0=>array(), 1=>array());
-			$extensions[1] = array(0=>array(), 1=>array());
-		}
 		foreach ($items as $item) {
-			$options = new JRegistry($item->options);
-			$item->image = $this->getBaseModel()->getMainImageUrl($item->image);
-			$item->downloadurl = $options->get($componentParams->get('fieldid_download_url'));
-			$item->fields = $options;
-			if ($search && preg_match('/^t2\.link_rating/', $order)) {
-				$extensions[1 - $item->foundintitle][$item->votelimit][] = $item;
-			}
-			elseif ($search) {
+			//print_r($item);
+			$item->image = $this->getBaseModel()->getMainImageUrl($item);
+			//$item->downloadurl = $options->get($componentParams->get('fieldid_download_url'));
+
+			if ($search) {
 				$extensions[1 - $item->foundintitle][] = $item;
-			}
-			elseif (preg_match('/^t2\.link_rating/', $order)) {
-				$extensions[0][$item->votelimit][] = $item;
 			}
 			else {
 				$extensions[0][] = $item;
 			}
 		}
-		
-		if (preg_match('/^t2\.link_rating\s+(\w+)/', $order, $matches)) {
-			if (strtoupper($matches[1]) == 'DESC') {
-				$extensions[0] = array_merge($extensions[0][1], $extensions[0][0]);
-				$extensions[1] = array_merge($extensions[1][1], $extensions[1][0]);
-			}
-			else {
-				$extensions[0] = array_merge($extensions[0][0], $extensions[0][1]);
-				$extensions[1] = array_merge($extensions[1][0], $extensions[1][1]);
-			}
-		}
 
 		return array_merge($extensions[0], $extensions[1]);
-		
+
 	}
-	
+
 	private function getAllChildren($children, $catid)
 	{
 		$allchildren = array();
@@ -379,12 +248,12 @@ class AppsModelCategory extends JModelList
 		}
 		return $allchildren;
 	}
-	
+
 	public function getCount()
 	{
 		return $this->_total;
 	}
-	
+
 	public function getPagination() {
 		$componentParams 	= JComponentHelper::getParams('com_apps');
 		$default_limit		= $componentParams->get('default_limit', 8);
@@ -395,7 +264,7 @@ class AppsModelCategory extends JModelList
 		$pagination->limitstart = $input->get('limitstart', 0, 'int');
 		$pagination->total		= $this->getCount();
 		$pagination->next		= $pagination->limitstart + $pagination->limit;
-		
+
 		return $pagination;
 
 	}
--- components/com_apps/models/dashboard.php
+++ components/com_apps/models/dashboard.php
@@ -35,19 +35,19 @@ class AppsModelDashboard extends JModelList
 	private $_parent = null;
 
 	private $_items = null;
-	
+
 	private $_remotedb = null;
-	
+
 	public $_extensionstitle = null;
 
 	private $_categories = null;
-	
+
 	private $_breadcrumbs = array();
-	
+
 	private $_total = null;
-	
+
 	private $_orderby = 'score';
-	
+
 	/**
 	 * Method to auto-populate the model state.
 	 *
@@ -134,22 +134,17 @@ class AppsModelDashboard extends JModelList
 		return $this->_parent;
 	}
 
-	private function getRemoteDB() {
-		$base_model = $this->getBaseModel();
-		return $base_model->getRemoteDB();
-	}
-	
 	private function getBaseModel()
 	{
 		$base_model = JModelLegacy::getInstance('Base', 'AppsModel');
 		return $base_model;
 	}
-	
+
 	private function getCatID()
 	{
 		return null;
 	}
-	
+
 	public function getCategories()
 	{
 		$base_model = $this->getBaseModel();
@@ -161,18 +156,18 @@ class AppsModelDashboard extends JModelList
 		$base_model = $this->getBaseModel();
 		return $base_model->getBreadcrumbs($this->getCatID());
 	}
-	
+
 	public function getPluginUpToDate()
 	{
 		$base_model = $this->getBaseModel();
 		return $base_model->getPluginUpToDate();
 	}
-	
+
 	public function getOrderBy()
 	{
 		return $this->_orderby;
 	}
-	
+
 	public function getExtensions()
 	{
 		// Get catid, search filter, order column, order direction
@@ -195,7 +190,7 @@ class AppsModelDashboard extends JModelList
 		$search 					= str_replace('_', ' ', urldecode(trim($input->get('filter_search', null))));
 
 		$release = intval($release / 5) * 5;
-		
+
 		$api_url->setScheme('http');
 		$api_url->setHost('extensions.joomla.org/index.php');
 		$api_url->setvar('option', 'com_jed');
@@ -209,97 +204,18 @@ class AppsModelDashboard extends JModelList
 		$api_url->setvar('extend', '0');
 		$api_url->setvar('order', $orderCol);
 		$api_url->setvar('dir', $orderDirn);
-		
+
 		if ($search)
 		$api_url->setvar('searchall', $search);
-		
-		//echo $api_url;
+
 		$extensions_json = $cache->call(array($http, 'get'), $api_url);
-		
-/*
-		// Get remote database
-		$db = $this->getRemoteDB();
-		
-		$query = 'SET SESSION group_concat_max_len=150000';
-		$db->setQuery($query);
-		$db->execute();
-		
-		$query = $db->getQuery(true);
-		$query->select(array('t2.link_id AS id'));
-		$query->from('jos_mt_links AS t2');
-		$query->join('RIGHT', 'jos_mt_cfvalues AS t3 ON t3.link_id = t2.link_id AND t3.cf_id = 37 AND ("'.$release.'" REGEXP t3.value OR t3.value = "")');
-
-		$where = array();
-		if ($search) {
-			$where[] = '(t2.link_name LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . ') OR t2.link_desc LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . '))';
-		}
-		
-		if (preg_match('/^t2\.link_rating/', $order)) {
-			$where[] = 't2.link_votes >= 15';
-		}
-
-		$where = array_merge($where, array(
-			't2.link_published = 1',
-			't2.link_approved = 1',
-			'(t2.publish_up <= NOW() OR t2.publish_up = "0000-00-00 00:00:00")',
-			'(t2.publish_down >= NOW() OR t2.publish_down = "0000-00-00 00:00:00")',
-		));
-		
-		$query->where($where);
-		$query->order($order);
-		$db->setQuery($query, $limitstart, $dashboard_limit);
-		$ids = $db->loadColumn();
-		
-		if ($search && !count($ids)) {
-			return array();
-		}
-
-		$query = $db->getQuery(true);
-		$fields = array(
-			't2.link_id AS id',
-			't2.link_id',
-			't2.link_name AS name',
-			't2.alias AS alias',
-			't2.link_desc AS description',
-			't2.link_rating AS rating',
-			't2.user_id AS user_id',
-			't2.link_votes',
-			't3.filename AS image',
-			't1.cat_id AS cat_id',
-			'CONCAT("{", GROUP_CONCAT(DISTINCT "\"", t5.cf_id, "\":\"", t4.value, "\""), "}") AS options',
-			'COUNT(DISTINCT t7.rev_id) AS reviews',
-		);
-		if ($search) {
-			$fields[] = 'IF(t2.link_name LIKE(' . $db->quote('%'.$db->escape($search, true).'%') . '), 1, 0) as foundintitle';
-		}
-		$query->select($fields);
-
-		$query->from('jos_mt_links AS t2');
-		$query->join('LEFT', 'jos_mt_cl AS t1 ON t1.link_id = t2.link_id');
-		$query->join('LEFT', 'jos_mt_images AS t3 ON t3.link_id = t2.link_id');
-		$query->join('LEFT', 'jos_mt_cfvalues AS t4 ON t2.link_id = t4.link_id');
-		$query->join('LEFT', 'jos_mt_customfields AS t5 ON t4.cf_id = t5.cf_id');
-		$query->join('LEFT', 'jos_mt_reviews AS t7 ON t7.link_id = t2.link_id');
-		
-		if (count($ids)) {
-			$query->where(array(
-				't2.link_id IN (' . implode(',', $ids) . ')',
-			));
-		}
-		$query->group('t2.link_id');
-		$query->order($order);
-		$db->setQuery($query);
-		$items = $db->loadObjectList();
-		*/
 
 		$items = json_decode($extensions_json->body);
 		$items = $items->data;
-		//$this->_total = count($items);
-	
-		
+
 		// Get CDN URL
 		$cdn = preg_replace('#/$#', '', trim($componentParams->get('cdn'))) . "/";
-		
+
 		// Populate array
 		$extensions = array(0=>array(), 1=>array());
 		foreach ($items as $item) {
@@ -314,9 +230,9 @@ class AppsModelDashboard extends JModelList
 				$extensions[0][] = $item;
 			}
 		}
-		
+
 		return array_merge($extensions[0], $extensions[1]);
-		
+
 	}
 
 	public function getCount()
