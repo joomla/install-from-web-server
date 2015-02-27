c29c245f3e792b678523eadad190165619bf6a5c
zero-24 <zero-24@users.noreply.github.com>
2015-02-27 08:56:13 +0100

    Update default.php

--- components/com_apps/views/dashboard/tmpl/default.php
+++ components/com_apps/views/dashboard/tmpl/default.php
@@ -1,35 +1,42 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  com_contact
+ * @package     InstallFromWebServer
+ * @subpackage  Site
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-$category_sidebar 		= new JLayoutFile('joomla.apps.category_sidebar');
-$extensions_imagegrid 	= new JLayoutFile('joomla.apps.extensions_imagegrid');
-$extensions_singlegrid 	= new JLayoutFile('joomla.apps.extensions_singlegrid');
-$advanced_search	 	= new JLayoutFile('joomla.apps.advanced_search');
-$simple_search			= new JLayoutFile('joomla.apps.simple_search');
-$extension_data			= array('extensions' => $this->extensions, 'breadcrumbs' => $this->breadcrumbs, 'params' => $this->params);
-$order_data			= array('orderby' => $this->orderby);
+$category_sidebar      = new JLayoutFile('joomla.apps.category_sidebar');
+$extensions_imagegrid  = new JLayoutFile('joomla.apps.extensions_imagegrid');
+$extensions_singlegrid = new JLayoutFile('joomla.apps.extensions_singlegrid');
+$advanced_search       = new JLayoutFile('joomla.apps.advanced_search');
+$simple_search         = new JLayoutFile('joomla.apps.simple_search');
+
+$extension_data = array(
+	'extensions'  => $this->extensions,
+	'breadcrumbs' => $this->breadcrumbs,
+	'params'      => $this->params,
+);
+
+$order_data = array(
+	'orderby' => $this->orderby,
+);
 ?>
 <div class="com-apps-container">
-<div class="row-fluid">
-	<div class="span3">
-		<?php echo $category_sidebar->render($this->categories); ?>
-	</div> 
-	<div class="span9">
-		<div class="row-fluid">
-			<div class="span12">
-				<?php echo $simple_search->render($order_data); ?>
+	<div class="row-fluid">
+		<div class="span3">
+			<?php echo $category_sidebar->render($this->categories); ?>
+		</div> 
+		<div class="span9">
+			<div class="row-fluid">
+				<div class="span12">
+					<?php echo $simple_search->render($order_data); ?>
+				</div>
 			</div>
+			<?php echo $extensions_imagegrid->render($extension_data); ?>
 		</div>
-
-		<?php echo $extensions_imagegrid->render($extension_data); ?>
 	</div>
-</div>
 </div>
