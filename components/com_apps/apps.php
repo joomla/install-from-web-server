fa303a0f8b2aa285a652f04352c814c8decb6400
zero-24 <zero-24@users.noreply.github.com>
2015-02-27 08:28:50 +0100

    Update apps.php

--- components/com_apps/apps.php
+++ components/com_apps/apps.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  com_apps
+ * @package     InstallFromWebServer
+ * @subpackage  Site
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
