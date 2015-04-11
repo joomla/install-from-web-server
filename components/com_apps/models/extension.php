818ade12fd20080ff7cd802c164ab71acc70760d
zero-24 <zero-24@users.noreply.github.com>
2015-04-11 22:45:02 +0200

    Update extension.php

--- components/com_apps/models/extension.php
+++ components/com_apps/models/extension.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  com_contact
+ * @package     InstallFromWebServer
+ * @subpackage  Site
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -12,23 +12,21 @@ defined('_JEXEC') or die;
 /**
  * This models supports retrieving lists of contact categories.
  *
- * @package     Joomla.Site
- * @subpackage  com_apps
- * @since       1.6
+ * @since  1.0.0
  */
 class AppsModelExtension extends JModelList
 {
 	/**
 	 * Model context string.
 	 *
-	 * @var		string
+	 * @var	string
 	 */
 	public $_context = 'com_apps.extension';
 
 	/**
 	 * The category context (allows other extensions to derived from this model).
 	 *
-	 * @var		string
+	 * @var	string
 	 */
 	protected $_extension = 'com_apps';
 
@@ -49,9 +47,9 @@ class AppsModelExtension extends JModelList
 	/**
 	 * Method to auto-populate the model state.
 	 *
-	 * Note. Calling getState in this method will result in recursion.
+	 * @note   Calling getState in this method will result in recursion.
 	 *
-	 * @since   1.6
+	 * @since  1.0.0
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
@@ -65,8 +63,8 @@ class AppsModelExtension extends JModelList
 		$params = $app->getParams();
 		$this->setState('params', $params);
 
-		$this->setState('filter.published',	1);
-		$this->setState('filter.access',	true);
+		$this->setState('filter.published', 1);
+		$this->setState('filter.access', true);
 	}
 
 	/**
@@ -78,15 +76,17 @@ class AppsModelExtension extends JModelList
 	 *
 	 * @param   string  $id	A prefix for the store id.
 	 *
-	 * @return  string  A store id.
+	 * @return  string  A store id
+	 *
+	 * @since   1.0.0
 	 */
 	protected function getStoreId($id = '')
 	{
 		// Compile the store id.
-		$id	.= ':'.$this->getState('filter.extension');
-		$id	.= ':'.$this->getState('filter.published');
-		$id	.= ':'.$this->getState('filter.access');
-		$id	.= ':'.$this->getState('filter.parentId');
+		$id .= ':'.$this->getState('filter.extension');
+		$id .= ':'.$this->getState('filter.published');
+		$id .= ':'.$this->getState('filter.access');
+		$id .= ':'.$this->getState('filter.parentId');
 
 		return parent::getStoreId($id);
 	}
@@ -94,28 +94,34 @@ class AppsModelExtension extends JModelList
 	/**
 	 * redefine the function an add some properties to make the styling more easy
 	 *
-	 * @return mixed An array of data items on success, false on failure.
+	 * @return  mixed  An array of data items on success, false on failure
+	 *
+	 * @since   1.0.0
 	 */
 	public function getItems()
 	{
 		if (!count($this->_items))
 		{
-			$app = JFactory::getApplication();
-			$menu = $app->getMenu();
+			$menu   = JFactory::getApplication()->getMenu();
 			$active = $menu->getActive();
 			$params = new JRegistry;
+
 			if ($active)
 			{
 				$params->loadString($active->params);
 			}
-			$options = array();
+
+			$options               = array();
 			$options['countItems'] = $params->get('show_cat_items_cat', 1) || !$params->get('show_empty_categories_cat', 0);
-			$categories = JCategories::getInstance('Contact', $options);
-			$this->_parent = $categories->get($this->getState('filter.parentId', 'root'));
+			$categories            = JCategories::getInstance('Contact', $options);
+			$this->_parent         = $categories->get($this->getState('filter.parentId', 'root'));
+
 			if (is_object($this->_parent))
 			{
 				$this->_items = $this->_parent->getChildren();
-			} else {
+			}
+			else
+			{
 				$this->_items = false;
 			}
 		}
@@ -129,13 +135,13 @@ class AppsModelExtension extends JModelList
 		{
 			$this->getItems();
 		}
+
 		return $this->_parent;
 	}
 
 	private function getBaseModel()
 	{
-		$base_model = JModelLegacy::getInstance('Base', 'AppsModel');
-		return $base_model;
+		return JModelLegacy::getInstance('Base', 'AppsModel');
 	}
 
 	private function getCatID()
@@ -145,33 +151,31 @@ class AppsModelExtension extends JModelList
 
 	public function getCategories()
 	{
-		$base_model = $this->getBaseModel();
-		return $base_model->getCategories($this->getCatID());
+		return $this->getBaseModel()->getCategories($this->getCatID());
 	}
 
 	public function getBreadcrumbs()
 	{
-		$base_model = $this->getBaseModel();
-		return $base_model->getBreadcrumbs($this->getCatID());
+		return $this->getBaseModel()->getBreadcrumbs($this->getCatID());
 	}
 
 	public function getPluginUpToDate()
 	{
-		$base_model = $this->getBaseModel();
-		return $base_model->getPluginUpToDate();
+		return $this->getBaseModel()->getPluginUpToDate();
 	}
 
 	public function getExtension()
 	{
 		// Get extension id
-		$cache 						= JFactory::getCache();
-		$cache->setCaching( 1 );
-		$http 						= new JHttp;
-		$api_url 					= new JUri;
-		$input 						= new JInput;
-		$id 							= $input->get('id', null, 'int');
-		$release 					= preg_replace('/[^\d]/', '', base64_decode($input->get('release', '', 'base64')));
-		$release 					= intval($release / 5) * 5;
+		$http    = new JHttp;
+		$api_url = new JUri;
+		$input   = new JInput;
+		$id      = $input->get('id', null, 'int');
+		$release = preg_replace('/[^\d]/', '', base64_decode($input->get('release', '', 'base64')));
+		$release = intval($release / 5) * 5;
+		$cache   = JFactory::getCache();
+
+		$cache->setCaching(1);
 
 		$api_url->setScheme('http');
 		$api_url->setHost('extensions.joomla.org/index.php');
@@ -187,20 +191,22 @@ class AppsModelExtension extends JModelList
 		$extension_json = $cache->call(array($http, 'get'), $api_url);
 
 		// Create item
-		$items = json_decode($extension_json->body);
-		$item = $items->data[0];
-		$this->_catid = $item->core_catid->value;
-		$item->image = $this->getBaseModel()->getMainImageUrl($item);
+		$items             = json_decode($extension_json->body);
+		$item              = $items->data[0];
+		$this->_catid      = $item->core_catid->value;
+		$item->image       = $this->getBaseModel()->getMainImageUrl($item);
 		$item->downloadurl = $item->download_integration_url->value;
-		if (preg_match('/\.xml\s*$/', $item->downloadurl)) {
-			$app = JFactory::getApplication();
-			$product = addslashes(base64_decode($app->input->get('product', '', 'base64')));
-			$release = preg_replace('/[^\d\.]/', '', base64_decode($app->input->get('release', '', 'base64')));
+
+		if (preg_match('/\.xml\s*$/', $item->downloadurl))
+		{
+			$app       = JFactory::getApplication();
+			$product   = addslashes(base64_decode($app->input->get('product', '', 'base64')));
+			$release   = preg_replace('/[^\d\.]/', '', base64_decode($app->input->get('release', '', 'base64')));
 			$dev_level = (int) base64_decode($app->input->get('dev_level', '', 'base64'));
 
 			$updatefile = JPATH_ROOT . '/libraries/joomla/updater/update.php';
-			$fh = fopen($updatefile, 'r');
-			$theData = fread($fh, filesize($updatefile));
+			$fh         = fopen($updatefile, 'r');
+			$theData    = fread($fh, filesize($updatefile));
 			fclose($fh);
 
 			$theData = str_replace('<?php', '', $theData);
@@ -213,18 +219,21 @@ class AppsModelExtension extends JModelList
 			$update = new JUpdate;
 			$update->loadFromXML($item->downloadurl);
 			$package_url_node = $update->get('downloadurl', false);
-			if (isset($package_url_node->_data)) {
+
+			if (isset($package_url_node->_data))
+			{
 				$item->downloadurl = $package_url_node->_data;
  			}
 		}
+
 		$item->download_type = $this->getTypeEnum($item->download_integration_type->value);
 
 		return array($item);
-
 	}
 
-	private function getTypeEnum($text) {
-		$options = array();
+	private function getTypeEnum($text)
+	{
+		$options    = array();
 		$options[0] = 'None';
 		$options[1] = 'Free Direct Download link:';
 		$options[2] = 'Free but Registration required at link:';
