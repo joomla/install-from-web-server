f1f545a3d988a607a6bdf01273ea5ca3291073de
Michael Babker <michael.babker@gmail.com>
2017-11-23 13:27:57 -0600

    PHP 5 is dead

--- components/com_apps/apps.php
+++ components/com_apps/apps.php
@@ -11,6 +11,11 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 
+if (!version_compare(PHP_VERSION, '7.0', 'ge'))
+{
+	throw new RuntimeException('The Install from Web server component requires PHP 7.0 or greater');
+}
+
 $controller = BaseController::getInstance('Apps');
 $controller->execute(Factory::getApplication()->input->get('task'));
 $controller->redirect();
