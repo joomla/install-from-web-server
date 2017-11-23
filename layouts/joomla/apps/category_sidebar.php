aa01e22f2d08968ca64566e77f4e2be4cb2e0bc8
Michael Babker <michael.babker@gmail.com>
2017-11-23 11:59:04 -0600

    Layout cleanup

--- layouts/joomla/apps/category_sidebar.php
+++ layouts/joomla/apps/category_sidebar.php
@@ -1,23 +1,37 @@
 <?php
 /**
- * @package     Joomla.CMS
- * @subpackage  Layout
+ * Joomla! Install From Web Server
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright  Copyright (C) 2013 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later
  */
 
 defined('JPATH_BASE') or die;
 
-$category_sidebar = new JLayoutFile('joomla.apps.category_sidebar_ul');
+use Joomla\CMS\Layout\FileLayout;
+
+$category_sidebar = new FileLayout('joomla.apps.category_sidebar_ul');
 ?>
 <div class="com-apps-sidebar">
 	<div class="scroll-pane well">
 		<ul class="nav nav-list">
 			<li class="nav-header"><h3><?php echo JText::_('COM_APPS_CATEGORIES'); ?></h3></li>
 			<?php foreach ($displayData as $category) : ?>
+				<?php
+				$ajaxUrlSegments = [];
+
+				if ($category->id)
+				{
+					$ajaxUrlSegments['view'] = 'category';
+					$ajaxUrlSegments['id']   = $category->id;
+				}
+				else
+				{
+					$ajaxUrlSegments['view'] = 'dashboard';
+				}
+				?>
 				<li<?php echo ($category->selected) ? ' class="active"' : ''; ?>>
-					<a class="transcode<?php echo $category->selected ? ' selected' : ''; ?>" href="<?php echo AppsHelper::getAJAXUrl(($category->id ? "view=category&id={$category->id}" : "view=dashboard")); ?>"><?php echo $category->name; ?></a>
+					<a class="transcode<?php echo $category->selected ? ' selected' : ''; ?>" href="<?php echo AppsHelper::getAJAXUrl($ajaxUrlSegments); ?>"><?php echo $category->name; ?></a>
 
 					<?php if ($category->active && count($category->children)) : ?>
 						<?php echo $category_sidebar->render($category->children); ?>
--- layouts/joomla/apps/category_sidebar_ul.php
+++ layouts/joomla/apps/category_sidebar_ul.php
@@ -1,20 +1,21 @@
 <?php
 /**
- * @package     Joomla.CMS
- * @subpackage  Layout
+ * Joomla! Install From Web Server
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright  Copyright (C) 2013 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later
  */
 
 defined('JPATH_BASE') or die;
 
-$category_sidebar = new JLayoutFile('joomla.apps.category_sidebar_ul');
+use Joomla\CMS\Layout\FileLayout;
+
+$category_sidebar = new FileLayout('joomla.apps.category_sidebar_ul');
 ?>
 <?php foreach ($displayData as $category) : ?>
 	<ul class="nav nav-list">
 		<li<?php echo ($category->selected) ? ' class="active"' : ''; ?>>
-			<a class="transcode<?php echo $category->selected ? ' selected' : ''; ?>" href="<?php echo AppsHelper::getAJAXUrl("view=category&id={$category->id}"); ?>"><?php echo $category->name; ?></a>
+			<a class="transcode<?php echo $category->selected ? ' selected' : ''; ?>" href="<?php echo AppsHelper::getAJAXUrl(['view' => 'category', 'id' => $category->id]); ?>"><?php echo $category->name; ?></a>
 
 			<?php if (count($category->children) && $category->active) : ?>
 				<?php echo $category_sidebar->render($category->children); ?>
