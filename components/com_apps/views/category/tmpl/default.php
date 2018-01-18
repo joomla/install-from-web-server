e70df4dd60315ad180b6a2932ef477e086473fda
Michael Babker <michael.babker@gmail.com>
2018-01-17 22:18:43 -0600

    Layout support for the two majors

--- components/com_apps/views/category/tmpl/default.php
+++ components/com_apps/views/category/tmpl/default.php
@@ -12,20 +12,23 @@ use Joomla\CMS\Layout\FileLayout;
 
 /** @var AppsViewCategory $this */
 
+$majorVersion = version_compare($this->release, '4.0', 'ge') ? 'j3' : 'j4';
+$layoutBase   = "apps.$majorVersion";
+
 ?>
 <div class="com-apps-container">
 	<div class="row-fluid">
 		<div class="span3">
-			<?php echo (new FileLayout('apps.category_sidebar'))->render($this->categories); ?>
+			<?php echo (new FileLayout($layoutBase . '.category_sidebar'))->render($this->categories); ?>
 		</div>
 		<div class="span9">
 			<div class="row-fluid">
 				<div class="span12">
-					<?php echo (new FileLayout('apps.simple_search'))->render(['orderby' => $this->orderby]); ?>
+					<?php echo (new FileLayout($layoutBase . '.simple_search'))->render(['orderby' => $this->orderby]); ?>
 				</div>
 			</div>
 
-			<?php echo (new FileLayout('apps.extensions_imagegrid'))->render(['extensions' => $this->extensions, 'breadcrumbs' => $this->breadcrumbs]); ?>
+			<?php echo (new FileLayout($layoutBase . '.extensions_imagegrid'))->render(['extensions' => $this->extensions, 'breadcrumbs' => $this->breadcrumbs]); ?>
 		</div>
 	</div>
 </div>
--- components/com_apps/views/extension/tmpl/default.php
+++ components/com_apps/views/extension/tmpl/default.php
@@ -12,20 +12,23 @@ use Joomla\CMS\Layout\FileLayout;
 
 /** @var AppsViewExtension $this */
 
+$majorVersion = version_compare($this->release, '4.0', 'ge') ? 'j3' : 'j4';
+$layoutBase   = "apps.$majorVersion";
+
 ?>
 <div class="com-apps-container">
 	<div class="row-fluid">
 		<div class="span3">
-			<?php echo (new FileLayout('apps.category_sidebar'))->render($this->categories); ?>
+			<?php echo (new FileLayout($layoutBase . '.category_sidebar'))->render($this->categories); ?>
 		</div>
 		<div class="span9">
 			<div class="row-fluid">
 				<div class="span12">
-					<?php echo (new FileLayout('apps.simple_search'))->render(); ?>
+					<?php echo (new FileLayout($layoutBase . '.simple_search'))->render(); ?>
 				</div>
 			</div>
 
-			<?php echo (new FileLayout('apps.extensions_full'))->render(['extension' => $this->extension, 'breadcrumbs' => $this->breadcrumbs]); ?>
+			<?php echo (new FileLayout($layoutBase . '.extensions_full'))->render(['extension' => $this->extension, 'breadcrumbs' => $this->breadcrumbs]); ?>
 		</div>
 	</div>
 </div>
