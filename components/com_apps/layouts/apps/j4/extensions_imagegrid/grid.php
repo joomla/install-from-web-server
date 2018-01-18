fdb1b14458ef85751767b4d22b8b92e5f96e216a
Michael Babker <michael.babker@gmail.com>
2018-01-18 17:19:56 -0600

    Use BS4 badges

--- components/com_apps/layouts/apps/j4/extensions_imagegrid/grid.php
+++ components/com_apps/layouts/apps/j4/extensions_imagegrid/grid.php
@@ -17,37 +17,37 @@ $commercial = $extension_data->type->value != "free" ? true : false;
 ?>
 <div class="item <?php echo $displayData['spanclass']; ?>">
 	<div class="card">
-		<div class="rating card-header">
+		<div class="rating card-header text-center">
 			<a target="_blank" href="<?php echo AppsHelper::getJEDUrl($extension_data) . '#reviews'; ?>"><?php echo Text::sprintf('COM_APPS_EXTENSION_VOTES_REVIEWS_LIST', $extension_data->score->value, $extension_data->num_reviews->value); ?></a>
 		</div>
 		<img src="<?php echo $extension_data->image; ?>" class="card-img-top" />
 		<div class="card-body" onclick="Joomla.loadweb(apps_base_url+'<?php echo AppsHelper::getAJAXUrl(['view' => 'extension', 'id' => $extension_data->id->value]); ?>');">
-			<h4 class="card-title"><?php echo trim($extension_data->core_title->value); ?></h4>
-			<ul class="item-type center">
-				<?php if ($commercial) : ?>
-					<span title="<?php echo $extension_data->type->value; ?>" class="label label-jcommercial">$</span>
-				<?php endif; ?>
-				<?php if (in_array('com', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_COMPONENT'); ?>" class="label label-jcomponent">C</span>
-				<?php endif; ?>
-				<?php if (in_array('lang', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_LANGUAGE'); ?>" class="label label-jlanguage">L</span>
-				<?php endif; ?>
-				<?php if (in_array('mod', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_MODULE'); ?>" class="label label-jmodule">M</span>
-				<?php endif; ?>
-				<?php if (in_array('plugin', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_PLUGIN'); ?>" class="label label-jplugin">P</span>
-				<?php endif; ?>
-				<?php if (in_array('esp', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_EXTENSION_SPECIFIC_ADDON'); ?>" class="label label-jspecial">S</span>
-				<?php endif; ?>
-				<?php if (in_array('tool', $tags)) : ?>
-					<span title="<?php echo Text::_('COM_APPS_TOOL'); ?>" class="label label-jtool">T</span>
-				<?php endif; ?>
-			</ul>
+			<h4 class="card-title text-center"><?php echo trim($extension_data->core_title->value); ?></h4>
 			<div class="card-text">
-				<?php echo HTMLHelper::_('string.truncate', $extension_data->core_body->value, 400, false, true); ?>
+				<div class="item-type text-center">
+					<?php if ($commercial) : ?>
+						<span title="<?php echo $extension_data->type->value; ?>" class="badge badge-warning">$</span>
+					<?php endif; ?>
+					<?php if (in_array('com', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_COMPONENT'); ?>" class="badge badge-success">C</span>
+					<?php endif; ?>
+					<?php if (in_array('lang', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_LANGUAGE'); ?>" class="badge badge-dark">L</span>
+					<?php endif; ?>
+					<?php if (in_array('mod', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_MODULE'); ?>" class="badge badge-danger">M</span>
+					<?php endif; ?>
+					<?php if (in_array('plugin', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_PLUGIN'); ?>" class="badge badge-secondary">P</span>
+					<?php endif; ?>
+					<?php if (in_array('esp', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_EXTENSION_SPECIFIC_ADDON'); ?>" class="badge badge-primary">S</span>
+					<?php endif; ?>
+					<?php if (in_array('tool', $tags)) : ?>
+						<span title="<?php echo Text::_('COM_APPS_TOOL'); ?>" class="badge badge-light">T</span>
+					<?php endif; ?>
+				</div>
+				<p><?php echo HTMLHelper::_('string.truncate', $extension_data->core_body->value, 400, false, true); ?></p>
 			</div>
 		</div>
 	</div>
