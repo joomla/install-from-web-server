c4a8ba829ffdd980f99253cb4f281e554543b1cc
Brian Teeman <brian@teeman.net>
2018-01-21 18:34:58 +0000

    Strings
    Sorry just spotted that we have strings for search in com_apps

--- components/com_apps/layouts/apps/j4/simple_search.php
+++ components/com_apps/layouts/apps/j4/simple_search.php
@@ -35,10 +35,10 @@ $selectedOrdering = $app->input->get('ordering', $orderby);
 		<div class="form-row">
 			<div class="col-md">
 				<div class="input-group">
-					<input type="text" name="filter_search" id="com-apps-searchbox" placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>" value="<?php echo $search; ?>" class="hasTooltip form-control">
+					<input type="text" name="filter_search" id="com-apps-searchbox" placeholder="<?php echo Text::_('COM_APPS_SEARCH_BTN'); ?>" value="<?php echo $search; ?>" class="hasTooltip form-control">
 					<div class="input-group-append">
-						<button type="button" class="btn btn-outline-secondary hasTooltip" onclick="Joomla.apps.initiateSearch();" data-original-title="<?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?>"><span class="fa fa-search" aria-hidden="true"></span></button>
-						<button type="button" class="btn btn-outline-secondary hasTooltip" data-original-title="<?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>" id="search-reset"><span class="fa fa-times" aria-hidden="true"></span></button>
+						<button type="button" class="btn btn-outline-secondary hasTooltip" onclick="Joomla.apps.initiateSearch();" data-original-title="<?php echo Text::_('COM_APPS_SEARCH_BTN'); ?>"><span class="fa fa-search" aria-hidden="true"></span></button>
+						<button type="button" class="btn btn-outline-secondary hasTooltip" data-original-title="<?php echo Text::_('COM_APPS_CANCEL_BTN'); ?>" id="search-reset"><span class="fa fa-times" aria-hidden="true"></span></button>
 					</div>
 				</div>
 			</div>
