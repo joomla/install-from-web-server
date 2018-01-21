f7bfe3bbc0852598d59333597aca355fa721f824
Michael Babker <michael.babker@gmail.com>
2018-01-21 11:21:27 -0600

    Responsive tweak

--- components/com_apps/layouts/apps/j4/simple_search.php
+++ components/com_apps/layouts/apps/j4/simple_search.php
@@ -33,7 +33,7 @@ $selectedOrdering = $app->input->get('ordering', $orderby);
 <div class="row">
 	<div id="filter-bar" class="col mb-3">
 		<div class="form-row">
-			<div class="col">
+			<div class="col-md">
 				<div class="input-group">
 					<input type="text" name="filter_search" id="com-apps-searchbox" placeholder="<?php echo Text::_('JSEARCH_FILTER'); ?>" value="<?php echo $search; ?>" class="hasTooltip form-control">
 					<div class="input-group-append">
@@ -43,10 +43,10 @@ $selectedOrdering = $app->input->get('ordering', $orderby);
 				</div>
 			</div>
 			<?php if ($view != 'extension') : ?>
-				<div class="col">
+				<div class="col-md">
 					<?php echo HTMLHelper::_('select.genericlist', $orderingOptions, 'ordering', 'class="custom-select"', 'value', 'text', $selectedOrdering, 'com-apps-ordering'); ?>
 				</div>
-				<div class="col">
+				<div class="col-md">
 					<div class="btn-group float-md-right">
 						<button type="button" class="btn btn-secondary grid-view<?php echo $list === 'grid' ? ' active' : ''; ?>" id="btn-grid-view"><span class="fa fa-th" aria-hidden="true"></span></button>
 						<button type="button" class="btn btn-secondary list-view<?php echo $list === 'list' ? ' active' : ''; ?>" id="btn-list-view"><span class="fa fa-list" aria-hidden="true"></span></button>
