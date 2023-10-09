97e4c09c66d2d2c9c749358e26ef0089493d9afa
Nicola Galgano <optimus4joomla@gmail.com>
2023-10-09 21:13:48 +0200

    Create default_j5.php

---
+++ components/com_apps/views/category/tmpl/default_j5.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Joomla! Install From Web Server
+ *
+ * @copyright  Copyright (C) 2013 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Layout\FileLayout;
+
+/** @var AppsViewCategory $this */
+
+?>
+<div class="com-apps-container">
+	<div class="row">
+		<div class="col-sm-4 col-md-3">
+			<?php echo (new FileLayout('apps.j5.category_sidebar'))->render($this->categories); ?>
+		</div>
+		<div class="col-sm-8 col-md-9">
+			<?php echo (new FileLayout('apps.j5.simple_search'))->render(['orderby' => $this->orderby]); ?>
+			<?php echo (new FileLayout('apps.j5.extensions_imagegrid'))->render(['extensions' => $this->extensions, 'breadcrumbs' => $this->breadcrumbs]); ?>
+		</div>
+	</div>
+</div>
